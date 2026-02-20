a4b4bb005344e6f5ef949bec5f8ea9179daaa2d0
vb-santos <vbs3@cesar.school>
2026-02-19 21:01:43 -0300

    C04 - Utils no projeto React

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -1,18 +1,68 @@
-import {PlayCircleIcon} from "lucide-react";
+import * as React from "react";
+import { useState } from "react";
+import { PlayCircleIcon } from "lucide-react";
+
+import { useTaskContext } from "../../hooks/useTaskContext.ts";
+
+import type { TaskModel } from "../../models/TaskModel.ts";
+
+import { getNextCycle } from "../../utils/getNextCycle.ts";
 
 import { Input } from "../Input";
 import { Button } from "../Button";
 import { Cycle } from "../Cycle";
 
+
 export const MainForm = () => {
+  const [taskName, setTaskName] = useState("")
+
+  const { setState, state } = useTaskContext();
+
+  const nextCycle = getNextCycle(state.currentCycle);
+
+  const handleCreatNewTask = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (taskName.trim() === "") {
+      alert("Digite o nome da tarefa!");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    }
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prevState.tasks, newTask],
+        config: { ...prevState.config },
+      }
+    });
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreatNewTask} className="form" action="">
       <div className="formRow">
         <Input
           type="text"
           id="task"
           labelText="Task"
           placeholder="Informe uma task."
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
         />
       </div>
 
